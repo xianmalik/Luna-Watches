03484c6a2cc642bb0299162375001c9b6ed598ac
Malik Zubayer <mzubayeruh@gmail.com>
2025-07-27 03:46:00 +0600

    Update header layout and refine search bar UI

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { MoonIcon, ShoppingCart, User } from "lucide-react";
+import { HamburgerIcon, MenuIcon, MoonIcon, ShoppingCart, User } from "lucide-react";
 
 // sub components
 import SearchBar from "@/components/common/navigation/SearchBar";
@@ -10,23 +10,29 @@ export default function Header() {
     return (
         <header className="text-black shadow-md p-4">
             {/* Primary Header */}
-            <div className="container mx-auto grid md:grid-cols-2 items-center px-2">
+            <div className="container mx-auto grid grid-cols-5 md:grid-cols-2 items-center px-2">
+                {/* Left Icons */}
+                <div className="flex md:hidden items-center gap-4">
+                    <MenuIcon />
+                </div>
+
                 {/* Logo */}
-                <Link href="/" className="text-2xl font-light uppercase flex items-center gap-2">
-                    <MoonIcon />
+                <Link href="/" className="text-2xl font-medium uppercase flex items-center justify-center md:justify-start gap-2 col-span-3 md:col-span-1">
                     {"Luna Watches"}
                 </Link>
 
                 {/* Right Icons */}
                 <div className="flex items-center justify-end gap-4">
-                    <SearchBar />
+                    <div className="hidden md:flex items-center gap-4">
+                        <SearchBar />
+                        <Link href="/cart"><ShoppingCart /></Link>
+                    </div>
                     <Link href="/login"><User /></Link>
-                    <Link href="/cart"><ShoppingCart /></Link>
                 </div>
             </div>
         
             {/* Navigation Menu */}
-            <nav className="container mx-auto mt-4">
+            <nav className="container mx-auto hidden md:block mt-4">
                 <ul className="flex space-x-4">
                     {NAVBAR_PRIMARY_ITEMS.map((item) => (
                         <li className="le-navbar--item" key={item.href}>
--- components/common/navigation/SearchBar.tsx
+++ components/common/navigation/SearchBar.tsx
@@ -5,8 +5,8 @@ export default function SearchBar() {
     return (
     <div className="flex w-full max-w-xs items-center relative">
       <Input
-        className="rounded-none pl-8"
-        placeholder="Search through our catalogue" />
+        className="rounded-none pl-8 placeholder:text-xs"
+        placeholder="Search Products" />
 
       <span className="absolute left-1 top-1/2 -translate-y-1/2 size-7 grid place-items-center">
         <SearchIcon className="h-4 w-4" />
